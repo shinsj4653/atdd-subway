14ad4d777b89592f654d58e5b7c6ec86903fe0af
singjun <shinsj4653@gmail.com>
2023-08-18 00:56:17 +0900

    feat: 공통적으로 사용할 생성시간, 수정시간을 관리할 BaseTimeEntity 생성

---
+++ src/main/java/kuit/subway/dto/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package kuit.subway.dto;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
