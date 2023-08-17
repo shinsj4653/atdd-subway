70693bcbf42461f2d28fb8f70906ae6f6100b20b
singjun <shinsj4653@gmail.com>
2023-08-18 01:08:38 +0900

    feat: LineDto 빌더 패턴 적용

--- src/main/java/kuit/subway/dto/response/line/LineDto.java
+++ src/main/java/kuit/subway/dto/response/line/LineDto.java
@@ -3,17 +3,24 @@ package kuit.subway.dto.response.line;
 import kuit.subway.domain.Station;
 import kuit.subway.dto.BaseTimeEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 public class LineDto extends BaseTimeEntity {
     private Long id;
     private String name;
     private String color;
     private List<Station> stations;
 
+    @Builder
+    public LineDto(Long id, String name, String color, List<Station> stations) {
+        this.id = id;
+        this.color = color;
+        this.name = name;
+        this.stations = stations;
+    }
 }
