a2b5cbf95f974bb08c9be394395be2d712e3ce04
singjun <shinsj4653@gmail.com>
2023-08-19 00:39:31 +0900

    fix: could not prepare statement [Column “ID” not found; SQL statement
    인수테스트 실행 시, 각 테이블마다 다른 식별자 값을 지니고 있을 때, 이에 대한 처리를 DatabaseCleanup에서 해줌

--- src/test/java/kuit/subway/study/line/LineAcceptanceTest.java
+++ src/test/java/kuit/subway/study/line/LineAcceptanceTest.java
@@ -3,11 +3,13 @@ package kuit.subway.study.line;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kuit.subway.AcceptanceTest;
+import org.junit.jupiter.api.DisplayName;
 
 import java.util.Map;
 
 import static kuit.subway.study.common.CommonRestAssured.post;
 
+@DisplayName("지하철 노선 인수 테스트")
 public class LineAcceptanceTest extends AcceptanceTest {
 
     public static final String STATION_PATH = "/stations";
