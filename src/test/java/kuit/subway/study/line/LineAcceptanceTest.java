4cace6944e78bc0ceeb5c8c052a7efc1a0d7f121
singjun <shinsj4653@gmail.com>
2023-08-19 01:53:37 +0900

    feat: 지하철 노선 수정 인수 테스트 완성

--- src/test/java/kuit/subway/study/line/LineAcceptanceTest.java
+++ src/test/java/kuit/subway/study/line/LineAcceptanceTest.java
@@ -7,6 +7,9 @@ import kuit.subway.AcceptanceTest;
 import kuit.subway.domain.Station;
 import kuit.subway.dto.request.line.CreateLineRequest;
 import kuit.subway.dto.request.station.CreateStationRequest;
+import kuit.subway.dto.response.line.UpdateLineResponse;
+import kuit.subway.study.common.CommonRestAssured;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,10 +17,9 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static kuit.subway.study.common.CommonRestAssured.get;
-import static kuit.subway.study.common.CommonRestAssured.post;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static io.restassured.RestAssured.put;
+import static kuit.subway.study.common.CommonRestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("지하철 노선 인수 테스트")
 public class LineAcceptanceTest extends AcceptanceTest {
@@ -44,7 +46,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         assertEquals(201, res.statusCode());
     }
 
-    @DisplayName("지하철 노선 조회 인수 테스트")
+    @DisplayName("지하철 노선 아이디 기반 조회 인수 테스트")
     @Test
     void getLineById() {
 
@@ -54,14 +56,59 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // when
         ExtractableResponse<Response> res = createDummyLine(station1, station2);
-        // then
         ExtractableResponse<Response> failedRes = get(LINE_PATH + "/" + 2);
-        assertEquals(400, failedRes.statusCode());
-
         ExtractableResponse<Response> successRes = get(LINE_PATH + "/" + res.jsonPath().getLong("id"));
-        assertEquals(200, successRes.statusCode());
+
+        // then
+        assertAll(
+                // 존재하지 않는 노선 조회 시 에러
+                () -> { assertEquals(400, failedRes.statusCode()); },
+                () -> { assertEquals(200, successRes.statusCode()); }
+        );
     }
-    
+
+    @DisplayName("지하철 노선 전체 목록 조회 인수 테스트")
+    @Test
+    void getAllLines() {
+
+        // given
+        CreateStationRequest station1 = new CreateStationRequest("강남역");
+        CreateStationRequest station2 = new CreateStationRequest("성수역");
+        CreateStationRequest station3 = new CreateStationRequest("논현역");
+        CreateStationRequest station4 = new CreateStationRequest("이수역");
+
+        // when
+        createDummyLine(station1, station2);
+        createDummyLine(station3, station4);
+        ExtractableResponse<Response> res = get(LINE_PATH);
+
+
+        // then
+        assertEquals(200, res.statusCode());
+    }
+
+    @DisplayName("지하철 노선 수정 인수 테스트")
+    @Test
+    void updateLine() {
+
+        // given
+        CreateStationRequest station1 = new CreateStationRequest("강남역");
+        CreateStationRequest station2 = new CreateStationRequest("성수역");
+        ExtractableResponse createdRes = createDummyLine(station1, station2);
+
+        // when
+        ExtractableResponse<Response> failedRes = get(LINE_PATH + "/" + 2);
+        CreateLineRequest req = new CreateLineRequest("red", 15, "신분당선", 2L, 1L);
+        ExtractableResponse<Response> updateRes = CommonRestAssured.put(LINE_PATH + "/" + createdRes.jsonPath().getLong("id"), req);
+
+        // then
+        assertAll(
+                // 존재하지 않는 노선 조회 시 에러
+                () -> { assertEquals(400, failedRes.statusCode()); },
+                () -> { assertEquals(200, updateRes.statusCode()); }
+        );
+    }
+
 
     private ExtractableResponse<Response> createDummyLine(CreateStationRequest downStation, CreateStationRequest upStation) {
 
