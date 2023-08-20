063f028f19befd79db4cc4aad58cef84520e6d54
singjun <shinsj4653@gmail.com>
2023-08-21 05:42:20 +0900

    feat: SectionAcceptanceTest 클래스 생성

---
+++ src/test/java/kuit/subway/study/section/SectionAcceptanceTest.java
@@ -0,0 +1,8 @@
+package kuit.subway.study.section;
+
+import kuit.subway.AcceptanceTest;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("지하철 구간 인수 테스트")
+public class SectionAcceptanceTest extends AcceptanceTest {
+}
