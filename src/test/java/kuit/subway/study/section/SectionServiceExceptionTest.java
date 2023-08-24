bc932c006e48b6deab6fde1b7154e58dd043d2d8
singjun <shinsj4653@gmail.com>
2023-08-24 23:29:41 +0900

    feat: 구간 추가 제약사항 반영한 Classic Test 완성 및 예외 테스트 파일 따로 생성

---
+++ src/test/java/kuit/subway/study/section/SectionServiceExceptionTest.java
@@ -0,0 +1,47 @@
+package kuit.subway.study.section;
+
+import jakarta.transaction.Transactional;
+import kuit.subway.domain.Line;
+import kuit.subway.domain.Section;
+import kuit.subway.domain.Station;
+import kuit.subway.repository.LineRepository;
+import kuit.subway.repository.StationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@DisplayName("지하철 구간 Classic 예외 테스트")
+@SpringBootTest
+@Transactional
+public class SectionServiceExceptionTest {
+
+    @Autowired
+    private LineRepository lineRepository;
+
+    @Autowired
+    private StationRepository stationRepository;
+
+    Station station1;
+    Station station2;
+    Station station3;
+    Line line;
+
+    // 사전에 필요한 역, 노선 데이터 생성
+    @BeforeEach
+    void setUpLine() {
+
+        station1 = Station.createStation("강남역");
+        station2 = Station.createStation("수서역");
+        station3 = Station.createStation("논현역");
+
+        stationRepository.save(station1);
+        stationRepository.save(station2);
+        stationRepository.save(station3);
+
+        line = Line.createLine("와우선", "green", 20);
+        line.addSection(Section.createSection(line, station1, station2, 10));
+        lineRepository.save(line);
+    }
+
+}
--- src/test/java/kuit/subway/study/section/SectionServiceTest.java
+++ src/test/java/kuit/subway/study/section/SectionServiceTest.java
@@ -4,13 +4,20 @@ import jakarta.transaction.Transactional;
 import kuit.subway.domain.Line;
 import kuit.subway.domain.Section;
 import kuit.subway.domain.Station;
+import kuit.subway.dto.response.station.StationDto;
 import kuit.subway.repository.LineRepository;
 import kuit.subway.repository.StationRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DisplayName("지하철 구간 Classic 테스트")
 @SpringBootTest
 @Transactional
@@ -21,26 +28,105 @@ public class SectionServiceTest {
 
     @Autowired
     private StationRepository stationRepository;
-    
-    // 구간 "만" 추가, 혹은 삭제하는 상황을 테스트 해주기 위해 미리 노선까지는 생성해주기
+
+    Station station1;
+    Station station2;
+    Station station3;
+    Line line;
+
+    // 사전에 필요한 역, 노선 데이터 생성
     @BeforeEach
     void setUpLine() {
         
-        Station upStation = Station.createStation("강남역");
-        Station downStation = Station.createStation("수서역");
-        stationRepository.save(upStation);
-        stationRepository.save(downStation);
-        
-        Line line = Line.createLine("와우선", "green", 20);
-        line.addSection(Section.createSection(line, upStation, downStation, 1));
+        station1 = Station.createStation("강남역");
+        station2 = Station.createStation("수서역");
+        station3 = Station.createStation("논현역");
+
+        stationRepository.save(station1);
+        stationRepository.save(station2);
+        stationRepository.save(station3);
+
+        line = Line.createLine("와우선", "green", 20);
+        line.addSection(Section.createSection(line, station1, station2, 10));
         lineRepository.save(line);
     }
 
     @DisplayName("지하철 구간 생성 - 새로운 역을 상행 종점으로 등록할 경우 맨 상위 구간으로 추가")
     @Test
-    void addSectionFirst() {
-        
+    void addSectionFirstUpStation() {
+
+        // given
+        line.addSection(Section.createSection(line, station3, station1, 5));
+
+        // when
+        List<StationDto> stationDtoList = line.getSections().getStationDtoList();
+
+        // then
+        assertEquals(station3.getId(), stationDtoList.get(0).getId());
+
     }
+
+    @DisplayName("지하철 구간 생성 - 새로운 역을 하행 종점으로 등록할 경우 맨 하위 구간으로 추가")
+    @Test
+    void addSectionLastDownStation() {
+
+        // given
+        line.addSection(Section.createSection(line, station2, station3, 5));
+
+        // when
+        List<StationDto> stationDtoList = line.getSections().getStationDtoList();
+
+        // then
+        assertEquals(station3.getId(), stationDtoList.get(stationDtoList.size() - 1).getId());
+
+    }
+
+    @DisplayName("사이에 끼울 경우 각 기존 구간의 상행역 or 하행역을 신규 구간 정보로 잘 변경 - 새로운 구간의 상행역이 이미 존재할 경우")
+    @Test
+    void addSectionBetweenStationsUpExist() {
+
+        // given
+        line.addSection(Section.createSection(line, station1, station3, 2));
+
+        // when
+        List<StationDto> stationDtoList = line.getSections().getStationDtoList();
+
+        // then
+        assertEquals(station3.getId(), stationDtoList.get(1).getId());
+        assertEquals(3, stationDtoList.size());
+    }
+
+    @DisplayName("사이에 끼울 경우 각 기존 구간의 상행역 or 하행역을 신규 구간 정보로 잘 변경 - 새로운 구간의 하행역이 이미 존재할 경우")
+    @Test
+    void addSectionBetweenStationsDownExist() {
+
+        // given
+        line.addSection(Section.createSection(line, station3, station2, 2));
+
+        // when
+        List<StationDto> stationDtoList = line.getSections().getStationDtoList();
+
+        // then
+        assertEquals(station3.getId(), stationDtoList.get(1).getId());
+        assertEquals(3, stationDtoList.size());
+    }
+
+    @DisplayName("노선 조회 시 상행 종점을 기준으로 역들이 잘 정렬되어 반환되어야 함")
+    @Test
+    void addSectionReturnAsSorted() {
+
+        // given
+        line.addSection(Section.createSection(line, station3, station2, 2));
+
+        // when
+        List<StationDto> stationDtoList = line.getSections().getStationDtoList();
+
+        // then
+        assertEquals(station1.getId(), stationDtoList.get(0).getId());
+        assertEquals(station3.getId(), stationDtoList.get(1).getId());
+        assertEquals(station2.getId(), stationDtoList.get(2).getId());
+    }
+
 
 
 }
