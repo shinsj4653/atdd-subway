0098b36f7eb96f8a0265625edb05f69275d8f2b0
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-09-04 17:46:22 +0900

    feat: Member 도메인 생성

---
+++ src/main/java/kuit/subway/domain/Member.java
@@ -0,0 +1,30 @@
+package kuit.subway.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer age;
+    private String email;
+    private String password;
+
+    public static Member of(Member member) {
+        return Member.builder()
+                .age(member.age)
+                .email(member.email)
+                .password(member.password)
+                .build();
+    }
+}
