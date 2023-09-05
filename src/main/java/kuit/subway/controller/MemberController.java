5a7e15ebd99621132fb8d22fd39354352abbc376
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-09-05 16:30:19 +0900

    feat: MemberController GET, POST, PUT, DELETE 요청 구현 완료

---
+++ src/main/java/kuit/subway/controller/MemberController.java
@@ -0,0 +1,46 @@
+package kuit.subway.controller;
+
+import kuit.subway.dto.request.line.LineCreateRequest;
+import kuit.subway.dto.request.line.LineUpdateRequest;
+import kuit.subway.dto.request.member.MemberRequest;
+import kuit.subway.dto.response.line.LineCreateResponse;
+import kuit.subway.dto.response.line.LineDeleteResponse;
+import kuit.subway.dto.response.line.LineReadResponse;
+import kuit.subway.dto.response.member.MemberResponse;
+import kuit.subway.service.LineService;
+import kuit.subway.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping()
+    public ResponseEntity<MemberResponse> createMember(@RequestBody MemberRequest request) {
+        MemberResponse res = memberService.createMember(request);
+        return ResponseEntity.created(URI.create("/members/" + res.getId()))
+                .body(res);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberResponse> findMember(@PathVariable("id") Long memberId) {
+        return ResponseEntity.ok(memberService.findMember(memberId));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MemberResponse> updateMember(@PathVariable("id") Long memberId, @RequestBody MemberRequest req) {
+        return ResponseEntity.ok(memberService.updateMember(memberId, req));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MemberResponse> deleteMember(@PathVariable("id") Long memberId) {
+        return ResponseEntity.ok(memberService.deleteMember(memberId));
+    }
+}
