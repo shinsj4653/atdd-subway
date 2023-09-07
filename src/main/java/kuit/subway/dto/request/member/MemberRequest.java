4321058ace1a499ae8b2c430d7dd2e37973c6457
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-09-07 10:18:19 +0900

    feat: MemberRequest 제약조건 추가

--- src/main/java/kuit/subway/dto/request/member/MemberRequest.java
+++ src/main/java/kuit/subway/dto/request/member/MemberRequest.java
@@ -1,8 +1,10 @@
 package kuit.subway.dto.request.member;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import kuit.subway.domain.Member;
 import lombok.*;
-import org.checkerframework.checker.index.qual.Positive;
 
 @Getter
 @Builder
@@ -13,10 +15,12 @@ public class MemberRequest {
     @Positive
     private Integer age;
 
-
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Pattern(regexp = "\\w+@\\w+\\.\\w+(\\.\\w+)?", message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[~!@#$%^&*()_+])[A-Za-z0-9~!@#$%^&*()_+]{8,}", message = "특수문자 포함 8자 이상 입력해야 합니다.")
     private String password;
 
     public Member toEntity() {
