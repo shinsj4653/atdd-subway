6a94ba3ff296aa683a64a865155286efb7816b15
singjun <shinsj4653@gmail.com>
2023-09-12 02:24:16 +0900

    refactor: 토큰 추출 -> AUTHENTICATION_TYPE 길이 활용

--- src/main/java/kuit/subway/auth/AuthorizationExtractor.java
+++ src/main/java/kuit/subway/auth/AuthorizationExtractor.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import kuit.subway.exception.badrequest.auth.BlankTokenException;
 import kuit.subway.exception.unauthorized.InvalidBearerException;
 import lombok.NoArgsConstructor;
+import org.apache.http.auth.AUTH;
 
 import java.util.Enumeration;
 
@@ -12,14 +13,13 @@ public class AuthorizationExtractor {
 
     private static final String AUTHENTICATION_TYPE = "Bearer";
     private static final String AUTHORIZATION_HEADER_KEY = "Authorization";
-    private static final int START_TOKEN_INDEX = 6;
 
     public static String extractAccessToken(HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaders(AUTHORIZATION_HEADER_KEY);
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
             if (value.toLowerCase().startsWith(AUTHENTICATION_TYPE.toLowerCase())) {
-                String extractToken = value.trim().substring(START_TOKEN_INDEX);
+                String extractToken = value.trim().substring(AUTHENTICATION_TYPE.length());
                 validateExtractToken(extractToken);
                 return extractToken;
             }
